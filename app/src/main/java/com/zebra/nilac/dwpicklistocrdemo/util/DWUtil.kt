73d711e3b251be525567bb4554a2d0ff3576a3cd
Calin <nilac8991@gmail.com>
2024-08-07 15:30:01 +0200

    Complete DW profile logic for Picklist OCR enablement
    Signed-off-by: Calin <nilac8991@gmail.com>

--- app/src/main/java/com/zebra/nilac/dwpicklistocrdemo/util/DWUtil.kt
+++ app/src/main/java/com/zebra/nilac/dwpicklistocrdemo/util/DWUtil.kt
@@ -69,7 +69,7 @@ object DWUtil {
         val bundlePluginConfig = ArrayList<Bundle>()
 
         val bPickListOcr = Bundle().apply {
-            putString("module_name", "MlKitExModule")
+            putString("module", "MlKitExModule")
             putBundle("module_params", Bundle().apply {
                 putString("session_timeout", "3000") //Integer Range  0 – 60000
                 putString("illumination", "off") //on - off
@@ -80,13 +80,40 @@ object DWUtil {
                 ) // 0 - Latin, 1 - Latin & Chinese, 2 - Latin and Japanese, 3 - Latin and Korean, 4 Latin and Devanagari
                 putString("confidence_level", "80") // Integer range 0-100
                 putString("text_structure", "0") // 0 - Single Word, 1- Single Line
+                putString(
+                    "picklist_mode",
+                    "0"
+                ) // 0 - OCR or Barcode, 1 - OCR Only, 2 - Barcode Only
+
+                putParcelableArrayList("rules",
+                    arrayListOf(
+                        Bundle().apply {
+                            putParcelableArrayList("rule_list", createOCRRules())
+                            putString("rule_param_id", "report_ocr_data")
+                        }
+                    )
+                )
+            })
+        }
+
+        val bPickListBarcode = Bundle().apply {
+            putString("module", "BarcodeDecoderModule")
+            putBundle("module_params", Bundle().apply {
+                putParcelableArrayList("rules",
+                    arrayListOf(
+                        Bundle().apply {
+                            putParcelableArrayList("rule_list", createBarcodeRules())
+                            putString("rule_param_id", "report_barcode_data")
+                        }
+                    )
+                )
             })
         }
 
         val bConfigWorkflowParamList = Bundle().apply {
             putString("workflow_name", "picklist_ocr")
             putString("workflow_input_source", "1")
-            putParcelableArrayList("workflow_params", arrayListOf(bPickListOcr))
+            putParcelableArrayList("workflow_params", arrayListOf(bPickListOcr, bPickListBarcode))
 
         }
         bConfigWorkflow.apply {
@@ -108,5 +135,68 @@ object DWUtil {
             setPackage("com.symbol.datawedge")
             putExtra("com.symbol.datawedge.api.SET_CONFIG", bMain)
         }
+    }
+
+    private fun createBarcodeRules(): ArrayList<Bundle> {
+        val ean8Rule = Bundle().apply {
+            putString("rule_name", "EAN8")
+            putBundle("criteria", Bundle().apply {
+                putParcelableArrayList(
+                    "identifier", arrayListOf(
+                        Bundle().apply {
+                            putString("criteria_key", "starts_with")
+                            putString("criteria_value", "58")
+                        }
+                    ))
+                putStringArray("symbology", arrayOf("decoder_ean8"))
+            })
+            putParcelableArrayList("actions", arrayListOf(
+                Bundle().apply {
+                    putString("action_key", "report")
+                    putString("action_value", "")
+                }
+            ))
+        }
+        return arrayListOf(ean8Rule)
+    }
+
+    private fun createOCRRules(): ArrayList<Bundle> {
+        val testOcrRule = Bundle().apply {
+            putString("rule_name", "TestOCR")
+            putBundle("criteria", Bundle().apply {
+                putParcelableArrayList(
+                    "identifier", arrayListOf(
+                        Bundle().apply {
+                            putString("criteria_key", "min_length")
+                            putString("criteria_value", "3")
+                        },
+                        Bundle().apply {
+                            putString("criteria_key", "max_length")
+                            putString("criteria_value", "7")
+                        },
+                        Bundle().apply {
+                            putString("criteria_key", "starts_with")
+                            putString("criteria_value", "A")
+                        },
+
+                        Bundle().apply {
+                            putString("criteria_key", "contains")
+                            putString("criteria_value", "BA")
+                        },
+
+                        Bundle().apply {
+                            putString("criteria_key", "ignore_case")
+                            putString("criteria_value", "true")
+                        })
+                )
+            })
+            putParcelableArrayList("actions", arrayListOf(
+                Bundle().apply {
+                    putString("action_key", "report")
+                    putString("action_value", "")
+                }
+            ))
+        }
+        return arrayListOf(testOcrRule)
     }
 }
